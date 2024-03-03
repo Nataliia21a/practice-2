37abe7369e51a5efbdedf5e80c4f324ed3772d9e
Nataliia21a <152820534+Nataliia21a@users.noreply.github.com>
2024-03-03 17:32:34 +0200

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -11,3 +11,43 @@ makeTransaction(10, 500);
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
 console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+
+// const start = 6;
+// const end = 17;
+// let number;
+// function numberFirst(start, end) {
+//   for (let index = 0; start < (index % 5 === 0) < end; index += 5) {
+//     number = start - (start % 5);
+//   }
+//   return number;
+// }
+
+// console.log(numberFirst(6, 17));
+
+// const start = 6;
+// const end = 17;
+// let number;
+// number = start - (start % 5);
+
+// console.log((number = start - (start % 5) + 5));
+
+// console.log(end - start);
+
+// for (let index = start; index <= end; index++) {
+//   if (index % 5 === 0) {
+//     number = index;
+//     break;
+//   }
+// }
+// console.log(number);
+
+function findNumber(start, end, divisor) {
+  let number;
+  for (let index = start; index <= end; index++) {
+    if (index % divisor === 0) {
+      return (number = index);
+    }
+  }
+}
+
+console.log(findNumber(8, 17, 3));
